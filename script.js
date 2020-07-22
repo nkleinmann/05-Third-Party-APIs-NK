fff2afa3357547f4fad9f0f9e14b6a1846e7f1f6
nkleinmann <nekleinmann@gmail.com>
2020-07-21 21:28:30 -0400

    Added JS for 1 rox

--- script.js
+++ script.js
@@ -5,4 +5,28 @@ $(document).ready(function() {
     console.log(todaysDate);
     $("#currentDay").text(todaysDate);
 
+
+
+
+    for (let i = 9; i < 10; i++) {
+            // Creates section for schedule
+    const rowEl = $('<div>').addClass("row time");
+    // Creates column with time
+    const columnTime = $('<div>').addClass("col-2 hour");
+    // Creates column with input
+    const columnInput = $('<div>').addClass("col-8 columnInput");
+    // // Creates column with font awesome
+    // const columnFA = $("<div></div>").addClass("col-2 columnFA");
+    // Creates button to save input
+    const saveButton = $('<button>').addClass("col-2 saveBtn i:hover");
+    saveButton.html('<i class="fa fa-save"></i>');
+
+        $("div.container").append(rowEl);
+        rowEl.append(columnTime, columnInput, saveButton);
+
+        columnTime.text(i + "AM");
+        
+    }
+
+
 });
