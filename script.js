eb86105ed5b50e93d592c2f3abb9c6100be43a81
nkleinmann <nekleinmann@gmail.com>
2020-07-24 09:02:08 -0400

    Color coding for time

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ $(document).ready(function () {
     // Using moment to get todays date; month, day, and year
     let todaysDate = moment().format('MMMM Do YYYY');
     $("#currentDay").text(todaysDate);
+
+    // Getting current time for color coding
+    let currentTime = moment().format("HH:mm");
+    console.log(currentTime);
+
+
+    
+
     // // Creates section for schedule
     // let rowEl = $('<div>').addClass("row time");
     // // Creates column with time
@@ -20,10 +28,20 @@ $(document).ready(function () {
     for (let i = 0; i < timeArray.length; i++) {
         // Creates section for schedule
         let rowEl = $('<div>').addClass("row time");
+
+        // getting correct time value for all times
+        let hourArray = timeArray[i].split(" ");
+        let hour = hourArray[0];
+        let newTime = hour;
+        if (hour < 6) {
+            newTime = hour + 12;
+        }
+       
+
         // Creates column with time
-        let columnTime = $('<div>').addClass("col-2 hour time-block");
+        let columnTime = $('<div>').addClass("col-2 hour time-block").data("value", newTime);
         // Creates column with input
-        let columnInput = $('<textarea>').addClass("col-8 columnInput bg-light");
+        let columnInput = $('<textarea>').addClass("col-8 columnInput");
         // Creates button to save input
         let saveButton = $('<button>').addClass("col-2 saveBtn i:hover");
         saveButton.html('<i class="fa fa-save"></i>');
@@ -36,11 +54,37 @@ $(document).ready(function () {
 
     };
 
+    // applying right class
+    function fctClass() {
+        console.log(moment().hour());
+        $(".time-block").each(function() {
+            
+            let textArea = $(this).siblings(".columnInput");
+            let textAreaHour = $(this).data("value");
+            if (textAreaHour < moment().hour()) {
+                console.log("Past");
+                textArea.addClass("past");
+            }
+            if (textAreaHour == moment().hour()) {
+                console.log("present");
+                textArea.addClass("present");
+            }
+            if (textAreaHour > moment().hour()) {
+                console.log("future");
+                textArea.addClass("future");
+            }
+        });
+    }
+    fctClass();
+
     // sets empty array
     let textInputArray = [];
 
     // storing input into local storage
-    function storeLS() {
+    // xt(textInputArray[i].text);
+    //         console.log(textInputArray[i].text);
+
+function storeLS() {
         textInputArray = JSON.parse(localStorage.getItem("planner"));
         let dailyPlan = $(".columnInput").val();
         let text = { dailyPlan };
@@ -65,10 +109,7 @@ $(document).ready(function () {
 
         // loops through textInputArray and displays daily plans on page
         for (let i = 0; i < textInputArray.length; i++) {
-            $(".textarea").text(textInputArray[i].text);
-            console.log(textInputArray[i].text);
-
-
+            $(".textarea").te
         }
         
         console.log(textInputArray);
